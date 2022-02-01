b6c288cfd8a0048ca6656fa468b3bd65ba00258a
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2022-02-01 12:34:12 +0300

    adding Middleware

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -1,35 +1,35 @@
 // let products = require("../../data")
 const Product = require("../../db/models/Product")
 
-exports.getProducts = async (req, res) => {
+exports.getProducts = async (req, res,next) => {
     try {
     const products = await Product.find({},{"color": 0,"quantity": 0,"createdAt": 0,"updatedAt": 0});
     res.json({ products });
     } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
     }
 };
 
-exports.getDetails = (req,res) => {
+exports.getDetails = (req,res,next) => {
     try {
     const { id } = req.params;
     const producto = products.find((e) => e.id === id)
     res.json(producto); 
     } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
     }
 };
 
-exports.postProducts = async (req, res) => {
+exports.postProducts = async (req, res, next) => {
     try {
     const newProduct = await Product.create(req.body)
     res.status(201).json(newProduct); 
     } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
     }
 };
 
-exports.deleteProductss = async (req, res) => {
+exports.deleteProductss = async (req, res, next) => {
     try {
     const { productId } = req.params;
     const findProduct = await Product.findByIdAndDelete({ _id: productId });
@@ -37,14 +37,14 @@ exports.deleteProductss = async (req, res) => {
     res.status(204).end();
     }
     else {
-    res.status(404).json({ message: "Product Not Found !"});
+    next({status:404, message: "Product not found" })
     }
     } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
     }
 };
 
-exports.updateProducts = async (req, res) => {
+exports.updateProducts = async (req, res,next) => {
     try{
     const { productId } = req.params;
     const product = await Product.findByIdAndUpdate({_id: productId}, req.body,{new: true, runValidators: true,})
@@ -52,10 +52,10 @@ exports.updateProducts = async (req, res) => {
     res.json(product);
     }
     else {
-    res.status(404).json({ message:"Product Not Found"})
+    next({status:404, message: "Product not found" })
     }
     }
     catch(error) {
-    res.status(500).json({ message: error.message });
+    next(error);
     }
 };
--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const res = require("express/lib/response");
 const productRouter = require("./apis/products/routes");
 const app = express();
 const connectDB = require("./db/database");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+console.log(`${req.method} ${req.protocol}://${req.get("host")}${req.path}`);
+next()
+});
+
 app.use("/api/products", productRouter)
 
+app.use((req, res) => {
+res.status(404).json({message: "Path Not Found"})
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ message: err.message || "Internal Server Error" });
+});
 
 connectDB();
 app.listen(8000, () => {
